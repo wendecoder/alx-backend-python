921a727b690cccf7ab6f12e24ae362b4c5bc3f14
wendecoder <wenonezra258@gmail.com>
2023-01-05 23:28:42 +0300

    fixed pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-""" A module that passes argument with annotation and
-returns the sum of the arguments
+""" A module that passes argument with annotation to a
+function
 """
 
 
 def add(a: float, b: float) -> float:
+    """ A function that returns the sum of
+    the arguments
+    """
     return a + b
