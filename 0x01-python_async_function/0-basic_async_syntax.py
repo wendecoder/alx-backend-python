1000facbdc1215cc5d3bf1062be05eb72e7de363
wendecoder <wenonezra258@gmail.com>
2023-01-09 13:15:53 +0300

    fixed the random error

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,6 +10,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """A function that returns the delay time"""
-    i = random.uniform(0, max_delay)
+    i = random.random() * max_delay
     await asyncio.sleep(i)
     return i
