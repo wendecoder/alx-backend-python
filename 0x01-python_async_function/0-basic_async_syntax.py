3a040165c93fa461fc8382800f92eab3a0f6d61c
wendecoder <wenonezra258@gmail.com>
2023-01-09 12:05:06 +0300

    completed first task

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# 0-basic_async_syntax.py
+"""
+A module that uses python asyncio package
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """A function that returns the delay time"""
+    i = random.uniform(0, max_delay)
+    await asyncio.sleep(i)
+    return i
