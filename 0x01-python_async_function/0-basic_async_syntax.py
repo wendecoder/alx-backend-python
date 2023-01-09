7eee84f63c6cb8eba0f39ae4a3104832f39ac8ba
wendecoder <wenonezra258@gmail.com>
2023-01-09 13:19:33 +0300

    fixed all the errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# 0-basic_async_syntax.py
+#!/usr/bin/env python3
 """
 A module that uses python asyncio package
 """
@@ -10,6 +9,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """A function that returns the delay time"""
-    i = random.random() * max_delay
+    i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
     return i
